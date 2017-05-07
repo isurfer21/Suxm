9f56ce8ca0c1316eb63507fcc86be69266fbd6fc
Abhishek Kumar <akbittu@gmail.com>
2017-05-07 13:35:21 +0530

    Stabilised the code

--- src/suxm.go
+++ src/suxm.go
@@ -5,18 +5,11 @@ import (
 	"fmt"
 	"github.com/kardianos/osext"
 	"log"
-	"net"
 	"net/http"
 	"os/exec"
 	"runtime"
+	"strconv"
 	"time"
-)
-
-var (
-	appDir, httpAddr string
-	webDir           = flag.String("dir", "/Users/", "root directory of www")
-	webSrv           = flag.String("srv", "127.0.0.1:8080", "host:port to listen on")
-	openBrowser      = flag.Bool("browser", true, "open browser automatically")
 )
 
 // WebService contains browser specific commands.
@@ -52,50 +45,65 @@ func (w WebService) startBrowser() bool {
 	return cmd.Start() == nil
 }
 
+var (
+	dirRoot, hostIP          string
+	portNum                  int
+	useSrvrRoot, openBrowser bool
+)
+
 func main() {
-	fmt.Println("\n Suxm webserver (Version 0.0.1) \n Copyright (c) 2017 Abhishek Kumar. All rights reserved. \n")
+	fmt.Println("\nSuxm webserver (Version 0.0.1) \nCopyright (c) 2017 Abhishek Kumar. All rights reserved. \n")
 
-	log.Print(*webDir)
-	log.Print(*webSrv)
-
-	currDir, err := osext.ExecutableFolder()
+	srvrRoot, err := osext.ExecutableFolder()
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	flag.BoolVar(&useSrvrRoot, "svr", false, "Use server root")
+	flag.StringVar(&dirRoot, "dir", "", "Directory root")
+	flag.StringVar(&hostIP, "host", "127.0.0.1", "Host IP or address")
+	flag.IntVar(&portNum, "port", 8080, "Port number")
+	flag.BoolVar(&openBrowser, "browser", true, "Open browser")
+
+	flag.Parse()
+
+	var appDir string
+	if useSrvrRoot == true {
+		appDir = srvrRoot
+		if dirRoot != "" {
+			appDir += dirRoot
+		}
 	} else {
-		if *webDir == "" {
-			appDir = currDir
+		if dirRoot != "" {
+			appDir = dirRoot
 		} else {
-			appDir = *webDir
+			switch runtime.GOOS {
+			case "darwin":
+				appDir = "/"
+			case "windows":
+				appDir = "c:\\"
+			default:
+				appDir = "/"
+			}
 		}
 	}
 
-	host, port, err := net.SplitHostPort(*webSrv)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if host == "" {
-		host = "localhost"
-	}
+	httpAddr := hostIP + ":" + strconv.Itoa(portNum)
 
-	httpAddr = host + ":" + port
-	if host != "127.0.0.1" && host != "localhost" {
-		fmt.Printf(" Warning: Address %s is not available. \n Please hit 'ctrl + C' to terminate this process.", httpAddr)
-	}
+	ws := WebService{}
+	ws.url = "http://" + httpAddr
 
 	go func() {
-		ws := WebService{}
-		ws.url = "http://" + httpAddr
-		if ws.waitServer() && *openBrowser && ws.startBrowser() {
-			fmt.Println(" A browser window should open. If not, visit the link.")
+		fmt.Println("Server status: STARTED")
+		if ws.waitServer() && openBrowser && ws.startBrowser() {
+			fmt.Println("A browser window should open. If not, visit the link.")
 		} else {
-			fmt.Println(" Please open your web browser and visit the link.")
+			fmt.Println("Please open your web browser and visit the link.")
 		}
-		fmt.Printf(" Serving at %s \n", ws.url)
-		fmt.Println(" Please hit 'ctrl + C' to terminate this process.")
+		fmt.Println("Please hit 'ctrl + C' to STOP the server.")
 	}()
 
-	fmt.Printf(" Serving from %s \n", appDir)
+	fmt.Println("Server settings \n  Root   ", appDir, "\n  URL    ", ws.url, "\n")
 	http.Handle("/", http.FileServer(http.Dir(appDir)))
-
 	http.ListenAndServe(httpAddr, nil)
 }
