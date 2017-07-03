3a6ca625b734faccdde1c416dd40865a090c0e63
Abhishek Kumar <akbittu@gmail.com>
2017-07-03 09:58:02 +0530

    Setup created for windows

--- src/setup.win.bat
+++ src/setup.win.bat
@@ -1,20 +1,32 @@
 @echo off
+INSTALLATION_DIR=%APPDATA%\Suxm
+CURRENT_DIR=%cd%
 echo.
-INSTALLATION_DIR=C:\Programs\
-CURRENT_DIR=$PWD
 echo Suxm webserver (Version 1.0.0)
 echo Copyright (c) 2017 Abhishek Kumar. All rights reserved.
 echo.
 echo Installing ...
-echo  - at $INSTALLATION_DIR
+echo  - at "%INSTALLATION_DIR%"
+echo  - via "%CURRENT_DIR%"
 echo  - creating installation directory
-mkdir -p $INSTALLATION_DIR
+if exist "%INSTALLATION_DIR%" (
+	echo  - removing previous installation
+	rmdir /s /q "%INSTALLATION_DIR%"
+)
+mkdir "%INSTALLATION_DIR%"
 echo  - copying the installation package
-cp -r $CURRENT_DIR/ $INSTALLATION_DIR/
-cd $INSTALLATION_DIR/bin
+xcopy /e "%CURRENT_DIR%" "%INSTALLATION_DIR%"
+cd "%INSTALLATION_DIR%\bin"
 echo  - giving short name to executable
-mv Suxm_darwin_amd64 Suxm
+if "%PROCESSOR_ARCHITECTURE%"=="x86" (
+	del Suxm_windows_amd64.exe
+	ren Suxm_windows_386.exe Suxm.exe
+) else (
+	del Suxm_windows_386.exe
+	ren Suxm_windows_amd64.exe Suxm.exe
+)
 echo  - making it globally accessible
-export PATH=$PATH:$PWD >> ~/.bash_profile
+setx PATH "$PATH:%cd%"
+cd "%CURRENT_DIR%"
 echo.
 echo Done!
